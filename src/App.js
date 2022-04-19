fc0ccd952b1ba859ea32876e815bead2ca59ae37
kartik5800 <kartiknavadiya198@gmail.com>
2022-04-19 08:34:45 +0530

    navigation link

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Containers/Home';
 import Footer from './Components/Footer/Footer';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Departments from "./Containers/Departments/Departments";
+import Doctors from './Containers/Doctors/Doctors';
+import About from './Containers/About/About';
+import Contact from './Containers/Contact/Contact';
 
 function App() {
   return (
     <div>
        <Header />
-       <Home />
+       <Switch>
+         <Route exact path={"/"} component={Home} />
+         <Route exact path={"/Departments"} component={Departments} />
+         <Route exact path={"/Doctors"} component={Doctors} />
+         <Route exact path={"/About"} component={About} />
+         <Route exact path={"/Contact"} component={Contact} />
+       </Switch>
        <Footer />
     </div>
   );
