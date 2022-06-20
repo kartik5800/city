8fceeaccbec0a41357eff6b035d34b86a2c369cd
kartik5800 <kartiknavadiya198@gmail.com>
2022-06-20 08:59:35 +0530

    20-6

--- src/Containers/Make_Appointment/Appointment.js
+++ src/Containers/Make_Appointment/Appointment.js
@@ -1,58 +1,105 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { boolean } from 'yup';
-import InputBox from '../../Components/InputBox/InputBox';
-import * as yup from 'yup';
-import { Form, Formik, useFormik } from 'formik';
+import React from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { boolean } from "yup";
+import InputBox from "../../Components/InputBox/InputBox";
+import * as yup from "yup";
+import { Form, Formik, useFormik } from "formik";
 
 function Appointment(props) {
+
+  const historydata= useHistory();
   let Make_Appointment = {
-    name: yup.string().required('enter name'),
-    email: yup.string().required('please enter email'),
-    phone: yup.string().required('please enter phone'),
-    date: yup.string().required('please enter date'),
-    department: yup.string().required('please enter department'),
-    message: yup.string().required('please enter message'),
-  }
+    name: yup.string().required("enter name"),
+    email: yup.string().required("please enter email"),
+    phone: yup.string().required("please enter phone"),
+    date: yup.string().required("please enter date"),
+    department: yup.string().required("please enter department"),
+    message: yup.string().required("please enter message"),
+  };
 
   let schema = yup.object().shape(Make_Appointment);
   const formik = useFormik({
     initialValues: {
-      name: '',
-      email: '',
-      phone: '',
-      date: '',
-      department: '',
-      message: '',
+      name: "",
+      email: "",
+      phone: "",
+      date: "",
+      department: "",
+      message: "",
     },
+
+
+
     validationSchema: schema,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+    onSubmit: (values) => {
+      // alert(JSON.stringify(values, null, 2));
+
+      const  {
+        name,
+         email,
+         phone,
+         date,
+         department
+       }=values
+       
+       
+         const apt={
+           // id:Math.floor(math.random() * 1000),
+           name,
+           email,
+           phone,
+           date,
+           department
+         }
+       
+       
+         let bookdata = JSON.parse(localStorage.getItem(Appointment));
+       
+       if (bookdata === null) {
+       localStorage.setItem("Appointment", JSON.stringify({apt}));
+       }else{
+         bookdata.push(apt)
+         localStorage.setItem("Appointment", JSON.stringify(bookdata));
+       }
+       
+       historydata.push("/")
+       
     },
   });
 
-  const{handleChange ,errors ,handleSubmit} = formik;
 
+
+
+
+  const { handleChange, errors, handleBlur, handleSubmit, touched } = formik;
 
   return (
     <section id="appointment" className="appointment">
       <div className="container">
         <div className="section-title">
           <h2>Make an Appointment</h2>
-          <p>Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc aliquam eget nibh eu euismod. Donec dapibus
-            blandit quam volutpat sollicitudin. Fusce tincidunt sit amet ex in volutpat. Donec lacinia finibus tortor.
-            Curabitur luctus eleifend odio. Phasellus placerat mi et suscipit pulvinar.</p>
+          <p>
+            Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc
+            aliquam eget nibh eu euismod. Donec dapibus blandit quam volutpat
+            sollicitudin. Fusce tincidunt sit amet ex in volutpat. Donec lacinia
+            finibus tortor. Curabitur luctus eleifend odio. Phasellus placerat
+            mi et suscipit pulvinar.
+          </p>
         </div>
-        <div className='row text-center'>
-          <div className='col-6'>
-            <NavLink activeClassName='aptnav' to={"/Make_Appointment"}>Make_Appointment</NavLink>
+        <div className="row text-center">
+          <div className="col-6">
+            <NavLink activeClassName="aptnav" to={"/Make_Appointment"}>
+              Make_Appointment
+            </NavLink>
           </div>
-          <div className='col-6'>
-            <NavLink activeClassName='aptnav' to={"/ListAppointment"}>List_Appointment</NavLink>
+          <div className="col-6">
+            <NavLink activeClassName="aptnav" to={"/ListAppointment"}>
+              List_Appointment
+            </NavLink>
           </div>
         </div>
-        <Formik value={formik}>
-          <Form onSubmit={handleSubmit}  className="php-email-form">
+        <Formik values={formik}>
+          <Form onSubmit={handleSubmit} className="php-email-form">
             <div className="row">
               <div className="col-md-4 form-group">
                 <InputBox
@@ -61,9 +108,10 @@ function Appointment(props) {
                   className="form-control"
                   id="name"
                   placeholder="Your Name"
-                  errors={Boolean(errors.name)}
-                  errormessage = {errors.name}
-                  onchange={handleChange}
+                  errors={Boolean(errors.name && touched.name)}
+                  errormessage={errors.name}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                 />
                 <div className="validate" />
               </div>
@@ -74,10 +122,11 @@ function Appointment(props) {
                   name="email"
                   id="email"
                   placeholder="Your Email"
-                  errors={Boolean(errors.email)}
-                  errormessage = {errors.email}
-                  onchange={handleChange}
-                  />
+                  errors={Boolean(errors.email && touched.email)}
+                  errormessage={errors.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
                 <div className="validate" />
               </div>
               <div className="col-md-4 form-group mt-3 mt-md-0">
@@ -87,9 +136,10 @@ function Appointment(props) {
                   name="phone"
                   id="phone"
                   placeholder="Your Phone"
-                  errors={Boolean(errors.phone)}
-                  errormessage = {errors.phone}
-                  onchange={handleChange}
+                  errors={Boolean(errors.phone && touched.phone)}
+                  errormessage={errors.phone}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                 />
                 <div className="validate" />
               </div>
@@ -102,42 +152,61 @@ function Appointment(props) {
                   className="form-control datepicker"
                   id="date"
                   placeholder="Appointment Date"
-                  errors={Boolean(errors.date)}
-                  errormessage = {errors.date}
-                  onchange={handleChange}
-                  />
+                  errors={Boolean(errors.date && touched.date)}
+                  errormessage={errors.date}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
                 <div className="validate" />
               </div>
               <div className="col-md-4 form-group mt-3">
-                <select name="department" id="department" className="form-select">
+                <InputBox
+                  type="select"
+                  name="department"
+                  id="department"
+                  className="form-select"
+                  errors={Boolean(errors.department && touched.department)}
+                  errormessage={errors.department}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                >
                   <option value>Select Department</option>
                   <option value="Department 1">Department 1</option>
                   <option value="Department 2">Department 2</option>
                   <option value="Department 3">Department 3</option>
-                </select>
+                </InputBox>
                 <div className="validate" />
               </div>
             </div>
             <div className="form-group mt-3">
-              <textarea
+              <InputBox
+                textarea
                 className="form-control"
                 name="message"
                 rows={5}
                 placeholder="Message (Optional)"
-                defaultValue={""} />
+                defaultValue={""}
+                errors={Boolean(errors.message && touched.message)}
+                errormessage={errors.message}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
               <div className="validate" />
             </div>
             <div className="mb-3">
               <div className="loading">Loading</div>
               <div className="errors-message" />
-              <div className="sent-message">Your appointment request has been sent successfully. Thank you!</div>
+              <div className="sent-message">
+                Your appointment request has been sent successfully. Thank you!
+              </div>
             </div>
-            <div className="text-center"><button type="submit">Make an Appointment</button></div>
+            <div className="text-center">
+              <button type="submit">Make an Appointment</button>
+            </div>
           </Form>
         </Formik>
       </div>
-    </section >
-
+    </section>
   );
 }
 
